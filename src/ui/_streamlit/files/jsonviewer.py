47413d150450e368a1d27089394952777d257c22
Lynn Hubbz <williamskyfurs@gmail.com>
2026-07-27 15:56:55 +0700

    fix(UI): Tidy up GUI placement for export page

--- src/ui/_streamlit/files/jsonviewer.py
+++ src/ui/_streamlit/files/jsonviewer.py
@@ -8,52 +8,51 @@ from ....core.filemanager import *
 
 jsonindent = 4
 
-def render_jsonviewer():
-    st.header("📄 Live Exported JSON")
+json_str = char.model_dump_json(indent=4)
 
-    json_str = char.model_dump_json(indent=4)
+assets_dict = st.session_state.get("pending_assets", {})
 
-    st.download_button(
-        label="💾 Download Character JSON",
-        data=json_str,
-        file_name="character.json",
-        mime="application/json",
-        use_container_width=True
-    )
+zip_bytes = create_character_bundle(
+    character_json=json_str,
+    assets=assets_dict
+)
 
+def render_exportpage():
+    
     st.caption("Saved file will contain direct key-value mapping with `null` defaults.")
+    
+    col1, col2 = st.columns([2,3], border=True)
 
+    with col1:
 
-    st.code(json_str, language="json")
+        st.header("Downloads")
 
+        st.download_button(
+            label="💾 Download Character JSON",
+            data=json_str,
+            file_name="character.json",
+            mime="application/json",
+            use_container_width=True
+        )
 
+            # 4. Download Button
+        st.download_button(
+            label="Download .zip Bundle",
+            data=zip_bytes,
+            file_name=f"{char.CHARACTER_IDENTITY.Names.full_name or 'character'}_bundle.zip",
+            mime="application/zip",
+            use_container_width=True
+        )
 
-def render_export_page():
-    st.header("📦 Export Character Package")
+        st.subheader("Settings")
 
-        # 1. Get JSON representation from Pydantic
-    char_json = char.model_dump_json(indent=4)
-    assets_dict = st.session_state.get("pending_assets", {})
+    with col2:
+        st.header("Previews")
 
-        # 3. Generate Zip File in Memory
-    zip_bytes = create_character_bundle(
-        character_json=char_json,
-        assets=assets_dict
-    )
-
-    # 4. Download Button
-    st.download_button(
-        label="Download .zip Bundle",
-        data=zip_bytes,
-        file_name=f"{char.CHARACTER_IDENTITY.names.full_name or 'character'}_bundle.zip",
-        mime="application/zip",
-        use_container_width=True
-    )
-
-    # 2. Preview ZIP Directory Structure
-    st.write("Directory Map Preview")
-    tree_lines = generate_directory_preview(list(assets_dict.keys()))
-    st.code("\n".join(tree_lines), language="text")
-
-
-
+        st.subheader("Directory Map Preview")
+        tree_lines = generate_directory_preview(list(assets_dict.keys()))
+        st.code("\n".join(tree_lines), language="text")
+    
+        st.subheader("📄 Live Exported JSON")
+        st.code(json_str, language="json")
+        
